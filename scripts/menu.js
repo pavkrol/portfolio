d600654f3fa1d87633b909ddd37d6b84e1340481
pavkrol <pdkrol@gmail.com>
2019-09-25 14:06:17 +0200

    change menu button behaviour on mobile and fixed header, add cv document

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,11 +4,9 @@ const nav_links = document.querySelectorAll(".navigation__link");
 
 menu_bttn.addEventListener("click", () => {
   nav_list.classList.toggle("navigation__list--active");
-  if (nav_list.classList.contains("navigation__list--active")) {
-    menu_bttn.setAttribute("aria-expanded", "true");
-  } else {
-    menu_bttn.setAttribute("aria-expanded", "false");
-  }
+    nav_list.classList.contains("navigation__list--active") 
+    ? menu_bttn.setAttribute("aria-expanded", "true")
+    : menu_bttn.setAttribute("aria-expanded", "false");
 });
 
 [...nav_links].forEach(el => {
