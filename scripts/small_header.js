b8cd6c2cc3614a1a999b0d1dd194a066c296a98c
pavkrol <pdkrol@gmail.com>
2019-12-02 21:29:20 +0100

    update project description, remove console.log from script

--- scripts/small_header.js
+++ scripts/small_header.js
@@ -1,9 +1,6 @@
 const header = document.querySelector(".header");
 
-
-
 window.addEventListener("scroll", () => {
-  console.log(window.pageYOffset);
   if (window.pageYOffset > 100) {
     header.classList.add("header--small");
   } else {
